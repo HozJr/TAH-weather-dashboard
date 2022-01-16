2cd25f724d95ddbf3ac07ce4a34840613fc8d2ce
HozJr <hozjr2006@gmail.com>
2022-01-16 17:40:18 -0600

    Const defined JS

---
+++ script.js
@@ -0,0 +1,19 @@
+function initPage() {
+    const inputEl = document.getElementById("city-input");
+    const searchEl = document.getElementById("search-button");
+    const clearEl = document.getElementById("clear-history");
+    const nameEl = document.getElementById("city-name");
+    const currentPicEl = document.getElementById("current-pic");
+    const currentTempEl = document.getElementById("temperature");
+    const currentHumidityEl = document.getElementById("humidity");4
+    const currentWindEl = document.getElementById("wind-speed");
+    const currentUVEl = document.getElementById("UV-index");
+    const historyEl = document.getElementById("history");
+    let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
+    console.log(searchHistory);
+    
+
+    const APIKey = "8922e9d4e8890ded6e4fb67524320d28";
+    //  Searched is clicked, Reads city name inputed
+    
+    function getWeather(cityName) {
